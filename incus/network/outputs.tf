f2eb69db97ebf4f574877fb051bc1e314db8c20a
Josias Leal <josias.leal@gmail.com>
2025-02-08 21:04:13 +0000

    Refactor Incus network output to correct DHCP range key and improve consistency

--- incus/network/outputs.tf
+++ incus/network/outputs.tf
@@ -1,12 +1,10 @@
 output "incus_network_details" {
   value = {
     name          = incus_network.network.name
-    ipv4_domain  = incus_network.network.config["ipv4.domain"]
-    ipv4_search  = incus_network.network.config["ipv4.search"]
     ipv4_address  = incus_network.network.config["ipv4.address"]
     ipv4_nat  = incus_network.network.config["ipv4.nat"]
     ipv4_dhcp  = incus_network.network.config["ipv4.dhcp"]
-    ipv4_dhcp_ranges  = incus_network.network.config["ipv4.dhcp.ranges"]
+    ipv4_dhcp_ranges  = incus_network.network.config["ipv4.dhcp_ranges"]
     ipv4_gateway  = incus_network.network.config["ipv4.dhcp.gateway"]
     routes           = incus_network.network.config["ipv4.routes"]
     routing           = incus_network.network.config["ipv4.routing"]
