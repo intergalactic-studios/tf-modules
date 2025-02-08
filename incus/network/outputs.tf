9043d85dcd0b962e81902cca3a6c50ce3c5e19a3
Josias Leal <josias.leal@gmail.com>
2025-02-08 21:02:58 +0000

    Refactor Incus network output variables for improved clarity and organization

--- incus/network/outputs.tf
+++ incus/network/outputs.tf
@@ -1,15 +1,14 @@
 output "incus_network_details" {
   value = {
     name          = incus_network.network.name
-    ipv4_address  = incus_network.network.config["ipv4.domain"]
-    ipv4_address  = incus_network.network.config["ipv4.search"]
+    ipv4_domain  = incus_network.network.config["ipv4.domain"]
+    ipv4_search  = incus_network.network.config["ipv4.search"]
     ipv4_address  = incus_network.network.config["ipv4.address"]
-    ipv4_address  = incus_network.network.config["ipv4.nat"]
-    ipv4_address  = incus_network.network.config["ipv4.dhcp"]
-    ipv4_address  = incus_network.network.config["ipv4.dhcp.ranges"]
-    ipv4_address  = incus_network.network.config["ipv4.dhcp.gateway"]
-    nat           = incus_network.network.config["ipv4.routes"]
-    nat           = incus_network.network.config["ipv4.routing"]
-    routing       = incus_network.network.config["ipv4.domain"]
+    ipv4_nat  = incus_network.network.config["ipv4.nat"]
+    ipv4_dhcp  = incus_network.network.config["ipv4.dhcp"]
+    ipv4_dhcp_ranges  = incus_network.network.config["ipv4.dhcp.ranges"]
+    ipv4_gateway  = incus_network.network.config["ipv4.dhcp.gateway"]
+    routes           = incus_network.network.config["ipv4.routes"]
+    routing           = incus_network.network.config["ipv4.routing"]
   }
 }
