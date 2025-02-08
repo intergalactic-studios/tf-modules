af1c038a1b537032e58a34fc67c01c7c04dd38e8
Josias Leal <josias.leal@gmail.com>
2025-02-08 21:05:09 +0000

    Remove unused DHCP ranges output from Incus network details for improved clarity

--- incus/network/outputs.tf
+++ incus/network/outputs.tf
@@ -4,7 +4,6 @@ output "incus_network_details" {
     ipv4_address  = incus_network.network.config["ipv4.address"]
     ipv4_nat  = incus_network.network.config["ipv4.nat"]
     ipv4_dhcp  = incus_network.network.config["ipv4.dhcp"]
-    ipv4_dhcp_ranges  = incus_network.network.config["ipv4.dhcp_ranges"]
     ipv4_gateway  = incus_network.network.config["ipv4.dhcp.gateway"]
     routes           = incus_network.network.config["ipv4.routes"]
     routing           = incus_network.network.config["ipv4.routing"]
