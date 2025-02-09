b1f85e25db3d42902aad0bfb85d30216ee2fb5df
Josias Leal <josias.leal@gmail.com>
2025-02-09 13:59:07 +0000

    test with null_resource

--- incus/network/main.tf
+++ incus/network/main.tf
@@ -4,3 +4,17 @@ resource "incus_network" "network" {
   description = var.description
   config = var.config
 }
+
+
+resource "null_resource" "test_script" {
+  # Runs when you apply Terraform
+  provisioner "local-exec" {
+    command = "echo 'Hello, Terraform!' > /home/intergalactic/testfile.txt"
+  }
+
+  # Runs when you destroy Terraform
+  provisioner "local-exec" {
+    when    = destroy
+    command = "rm -f /tmp/testfile.txt"
+  }
+}
