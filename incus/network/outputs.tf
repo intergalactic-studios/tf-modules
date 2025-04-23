e379382fddf1f1e36a11a6e951cb0bfadb7b8a80
Josias Leal <josias.leal@gmail.com>
2025-04-23 19:20:15 +0100

    Comment out ipv4_address in incus_network_details output for clarity

--- incus/network/outputs.tf
+++ incus/network/outputs.tf
@@ -1,7 +1,7 @@
 output "incus_network_details" {
   value = {
     name          = incus_network.incusbr0.name
-    ipv4_address  = incus_network.incusbr0.config["ipv4.address"]
+    #ipv4_address  = incus_network.incusbr0.config["ipv4.address"]
     type          = incus_network.incusbr0.type
     description   = incus_network.incusbr0.description
     # ipv4_nat  = incus_network.incusbr0.config["ipv4.nat"]
