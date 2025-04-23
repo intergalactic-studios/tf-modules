8d706bd1711e35a989a088ea1be88cac8c799533
Josias Leal <josias.leal@gmail.com>
2025-04-23 19:18:51 +0100

    Update output configuration to reference incus_network.bridge0 instead of incus_network.network

--- incus/network/outputs.tf
+++ incus/network/outputs.tf
@@ -1,13 +1,13 @@
 output "incus_network_details" {
   value = {
-    name          = incus_network.network.name
-    ipv4_address  = incus_network.network.config["ipv4.address"]
-    type          = incus_network.network.type
-    description   = incus_network.network.description
-    # ipv4_nat  = incus_network.network.config["ipv4.nat"]
-    # ipv4_dhcp  = incus_network.network.config["ipv4.dhcp"]
-    # #ipv4_gateway  = incus_network.network.config["ipv4.dhcp.gateway"]
-    # routes           = incus_network.network.config["ipv4.routes"]
-    # routing           = incus_network.network.config["ipv4.routing"]
+    name          = incus_network.bridge0.name
+    ipv4_address  = incus_network.bridge0.config["ipv4.address"]
+    type          = incus_network.bridge0.type
+    description   = incus_network.bridge0.description
+    # ipv4_nat  = incus_network.bridge0.config["ipv4.nat"]
+    # ipv4_dhcp  = incus_network.bridge0.config["ipv4.dhcp"]
+    # #ipv4_gateway  = incus_network.bridge0.config["ipv4.dhcp.gateway"]
+    # routes           = incus_network.bridge0.config["ipv4.routes"]
+    # routing           = incus_network.bridge0.config["ipv4.routing"]
   }
 }
