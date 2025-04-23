4ef839844ada115576e040c6a1f14914c749efc1
Josias Leal <josias.leal@gmail.com>
2025-04-23 19:17:10 +0100

    Refactor network configuration by renaming incus_network resource and removing unused ACL and provisioner files

--- incus/network/main.tf
+++ incus/network/main.tf
@@ -1,15 +1,7 @@
-resource "incus_network" "network" {
+resource "incus_network" "incusbr0" {
   name = var.name
   type = var.type
   description = var.description
   config = var.config
 
-
-  depends_on = [null_resource.disable_eno1]
-
-  lifecycle {
-    ignore_changes = [
-      config["bridge.external_interfaces"], # Ignore changes to avoid conflicts when disabling eno1
-    ]
-  }
 }
--- incus/network/outputs.tf
+++ incus/network/outputs.tf
@@ -1,7 +1,9 @@
 output "incus_network_details" {
   value = {
     name          = incus_network.network.name
-    # ipv4_address  = incus_network.network.config["ipv4.address"]
+    ipv4_address  = incus_network.network.config["ipv4.address"]
+    type          = incus_network.network.type
+    description   = incus_network.network.description
     # ipv4_nat  = incus_network.network.config["ipv4.nat"]
     # ipv4_dhcp  = incus_network.network.config["ipv4.dhcp"]
     # #ipv4_gateway  = incus_network.network.config["ipv4.dhcp.gateway"]
