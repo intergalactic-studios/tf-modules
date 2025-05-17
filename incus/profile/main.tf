c1ce8365cb34715163042eec1784d0587930e3b9
Josias Leal <josias.leal@gmail.com>
2025-05-17 15:15:10 +0100

    Refactor incus profile to use dynamic devices block and update variable definitions for improved flexibility

--- incus/profile/main.tf
+++ incus/profile/main.tf
@@ -2,5 +2,14 @@ resource "incus_profile" "profile" {
   name = var.name
   description = var.description
   config = var.config
-  device = var.device
+  
+  # The devices block is a dynamic block that allows you to create multiple
+  dynamic "device" {
+    for_each = var.devices
+    content {
+      name       = device.value.name
+      type       = device.value.type
+      properties = device.value.properties
+    }
+  }
 }
--- incus/profile/variables.tf
+++ incus/profile/variables.tf
@@ -3,9 +3,24 @@ variable "name" {
   type        = string
 }
 
-variable "device" {
-  type        = map(string)
-  description = "The device of the network"
+variable "devices" {
+  description = "List of devices to add to the profile"
+  type = list(object({
+    name       = string
+    type       = string
+    properties = map(string)
+  }))
+  default = []
+  validation {
+    condition = alltrue([
+      for d in var.devices : contains([
+        "none", "disk", "nic", "unix-char", "unix-block", 
+        "usb", "gpu", "infiniband", "proxy", 
+        "unix-hotplug", "tpm", "pci"
+      ], d.type)
+    ])
+    error_message = "Device type must be one of the supported Incus device types"
+  }
 }
 
 variable "config" {
