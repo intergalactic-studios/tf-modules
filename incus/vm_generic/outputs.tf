4487956e55443020029668a38bb7fea6c1257ba7
Josias Leal <josias.leal@gmail.com>
2025-06-11 23:38:55 +0100

    Remove 'addresses' output from vm_generic module and delete obsolete cloud-init template

--- incus/vm_generic/outputs.tf
+++ incus/vm_generic/outputs.tf
@@ -2,10 +2,6 @@ output "vm_name" {
   value = incus_instance.this.name
 }
 
-output "addresses" {
-  value = incus_instance.this.addresses
-}
-
 output "config" {
   value = incus_instance.this.config
 }
