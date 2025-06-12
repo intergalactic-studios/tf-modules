26a2643eadc17d00a140b51f905b055bc2a044f7
Josias Leal <josias.leal@gmail.com>
2025-06-12 14:30:48 +0100

    Fix backups variable reference in vultr_instance resource for consistency

--- vultr/instance/main.tf
+++ vultr/instance/main.tf
@@ -13,7 +13,7 @@ resource "vultr_instance" "this" {
 
   enable_ipv6          = var.enable_ipv6
   disable_public_ipv4  = var.disable_public_ipv4
-  backups              = var.enable_backups
+  backups              = var.backups
   ddos_protection      = var.enable_ddos_protection
   activation_email     = var.activation_email
 
