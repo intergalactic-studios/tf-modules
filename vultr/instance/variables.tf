72894ec0fba3e6d437c64809dd91cc87c44a6b74
Josias Leal <josias.leal@gmail.com>
2025-06-12 14:19:40 +0100

    Add new variables for ISO ID, application ID, user data, and instance configurations

--- vultr/instance/variables.tf
+++ vultr/instance/variables.tf
@@ -42,3 +42,69 @@ variable "tags" {
     type        = list(string)
     default     = []
 }
+
+variable "iso_id" {
+    description = "The ISO ID to use for the instance (optional)."
+    type        = number
+    default     = null
+}
+
+variable "app_id" {
+    description = "The application ID to use for the instance (optional)."
+    type        = number
+    default     = null
+}
+
+variable "user_data" {
+    description = "Cloud-init user data to use for the instance (optional)."
+    type        = string
+    default     = null
+}
+
+variable "enable_ipv6" {
+    description = "Enable IPv6 for the instance."
+    type        = bool
+    default     = false
+}
+
+variable "disable_public_ipv4" {
+    description = "Disable public IPv4 for the instance."
+    type        = bool
+    default     = false
+}
+
+variable "enable_backups" {
+    description = "Enable automatic backups for the instance."
+    type        = bool
+    default     = false
+}
+
+variable "enable_ddos_protection" {
+    description = "Enable DDoS protection for the instance."
+    type        = bool
+    default     = false
+}
+
+variable "activation_email" {
+    description = "Send activation email for the instance."
+    type        = bool
+    default     = false
+}
+
+variable "firewall_group_id" {
+    description = "The firewall group ID to assign to the instance (optional)."
+    type        = string
+    default     = null
+}
+
+variable "script_id" {
+    description = "The script ID to assign to the instance (optional)."
+    type        = string
+    default     = null
+}
+
+variable "reserved_ip_id" {
+    description = "The reserved IP ID to assign to the instance (optional)."
+    type        = string
+    default     = null
+}
