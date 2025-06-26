dbead8321a398b9b5b78427a75e7b5fdccfb6790
Josias Leal <josias.leal@gmail.com>
2025-06-26 20:56:13 +0100

    Remove unused labels block and variable from portainer_docker_secret resource

--- portainer/secrets/main.tf
+++ portainer/secrets/main.tf
@@ -2,18 +2,7 @@ resource "portainer_docker_secret" "example_secret" {
   endpoint_id = var.endpoint_id
   name        = var.secret_name
   data        = var.secret_data
-
-#   labels = {
-#     "com.example.some-label" = "some-value"
-#   }
-  # Common env vars
-  dynamic "labels" {
-    for_each = var.labels
-    content {
-      name  = env.value.name
-      value = env.value.value
-    }
-  }
+    
 
 }
 
@@ -28,9 +17,4 @@ variable "secret_name" {
 variable "secret_data" {
   description = "The data for the secret, base64 encoded."
   type        = string
-}
-variable "labels" {
-  description = "A map of labels to apply to the secret."
-  type        = map(string)
-  default     = {}
 }
